65c11652d1412b016c2bf2b7800f54c57e770b32
Chiemerie <alexemerie7@gmail.com>
2025-10-21 23:21:52 +0100

    Created auth service and shared database library

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -6,6 +6,8 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { database } from '@card-hive/shared-database';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +15,7 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
+  Logger.log(`Database log: ${database()}`);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
 
