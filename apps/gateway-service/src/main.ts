12b8747742c30fa3071251ed33191aa60405e936
Chiemerie <alexemerie7@gmail.com>
2025-11-02 12:20:09 +0000

    Set global prefix for api gateway service

--- apps/gateway-service/src/main.ts
+++ apps/gateway-service/src/main.ts
@@ -7,6 +7,7 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
+  app.setGlobalPrefix('api');
 
   const config = app.get(ConfigService);
 
