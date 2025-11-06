6279709c9047545a051bb578d6795a7089550e0c
Chiemerie <alexemerie7@gmail.com>
2025-11-06 11:16:54 +0000

    Removed database_url string generation in docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-export DATABASE_URL="postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
-
 pnpm dlx prisma generate --schema=./libs/shared/database/prisma/schema.prisma
 pnpm dlx prisma migrate deploy --schema=./libs/shared/database/prisma/schema.prisma
 
