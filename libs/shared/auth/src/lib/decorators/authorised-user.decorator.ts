db2cf12c8c919e2d65de6a0401a9e0e92ab3b90e
Chiemerie <alexemerie7@gmail.com>
2025-11-14 14:03:54 +0000

    Changed variable name in authuser decoratore from data to key

--- libs/shared/auth/src/lib/decorators/authorised-user.decorator.ts
+++ libs/shared/auth/src/lib/decorators/authorised-user.decorator.ts
@@ -3,12 +3,12 @@ import { IAuthorisedUser } from '../types.js';
 
 
 export const AuthorisedUser = createParamDecorator(
-  (data: keyof IAuthorisedUser, ctx: ExecutionContext) => {
+  (key: keyof IAuthorisedUser, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     const user: IAuthorisedUser = request.user;
 
-    if (data) {
-      return user?.[data];
+    if (key) {
+      return user?.[key];
     }
 
     return { ...user };
