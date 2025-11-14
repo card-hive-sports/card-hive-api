e49f335797a617c46e41ca65dfed2e4c6518af17
Chiemerie <alexemerie7@gmail.com>
2025-11-14 20:40:59 +0000

    Added shared database and shared auth module

--- apps/inventory-service/src/app/inventory.module.ts
+++ apps/inventory-service/src/app/inventory.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { InventoryController } from './inventory.controller';
 import { InventoryService } from './inventory.service';
+import { ConfigModule } from '@nestjs/config';
+import { inventoryConfig } from './config/inventory.config';
+import { SharedAuthModule } from "@card-hive/shared-auth";
+import { SharedDatabaseModule } from "@card-hive/shared-database";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [inventoryConfig],
+      cache: true,
+      expandVariables: true,
+    }),
+    SharedAuthModule,
+    SharedDatabaseModule,
+  ],
   controllers: [InventoryController],
   providers: [InventoryService],
 })
